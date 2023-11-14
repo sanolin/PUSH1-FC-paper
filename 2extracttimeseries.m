e4df555e564fd4d32bd511f9d1577ae38933deb1
sanolin <150274849+sanolin@users.noreply.github.com>
2023-11-14 15:03:09 -0500

    Update 2extracttimeseries.m

--- 2extracttimeseries.m
+++ 2extracttimeseries.m
@@ -1,10 +1,23 @@
-%load mgh surface overlap files
-R=load_mgh('BOLDsurfrh.mgh');
-L=load_mgh('BOLDsurflh.mgh');
+%loads surface overlays of cleaned bold data, pulls out timeseries, and concatenates 3 scans
+rhsurfs = dir('/data/project/vislab/a/HCP_diff_Sara/wholeFNstoallV1tprob/wholeFNstoallV1t_biggest_surf/fsavg*rh*');
 
-%created plot of data if wanted
-%stackedplot(L);
-
-%pull out timeseries data from 4d array mgh
-timeseriesL =squeeze(L);
-timeseriesR=squeeze(R);
+for ii = 1:length(lhsurfs)
+for patient in 
+do
+  for scan in 1:3
+  do
+  %load mgh surface overlap files
+  R=load_mgh('BOLDsurfrh.mgh');
+  L=load_mgh('BOLDsurflh.mgh');
+  
+  %created plot of data if wanted
+  %stackedplot(L);
+  
+  %pull out timeseries data from 4d array mgh
+  timeseriesL =squeeze(L);
+  timeseriesR=squeeze(R);
+  
+  %concatenate 3 scans
+  
+  done
+done
