f141b680b4e78598243426abffb17406b9709889
sanolin <150274849+sanolin@users.noreply.github.com>
2023-11-14 15:12:01 -0500

    Update 2extracttimeseries.m

--- 2extracttimeseries.m
+++ 2extracttimeseries.m
@@ -1,23 +1,26 @@
 %loads surface overlays of cleaned bold data, pulls out timeseries, and concatenates 3 scans
-rhsurfs = dir('/data/project/vislab/a/HCP_diff_Sara/wholeFNstoallV1tprob/wholeFNstoallV1t_biggest_surf/fsavg*rh*');
+patients = dir('/Volumes/vdrive/helpern_users/benitez_a/PUSH/PUSH_1/PUSH_Analyses/fMRI_Analysis/Sara/02_Data/P*');
 
-for ii = 1:length(lhsurfs)
-for patient in 
-do
-  for scan in 1:3
-  do
-  %load mgh surface overlap files
-  R=load_mgh('BOLDsurfrh.mgh');
-  L=load_mgh('BOLDsurflh.mgh');
-  
-  %created plot of data if wanted
-  %stackedplot(L);
-  
-  %pull out timeseries data from 4d array mgh
-  timeseriesL =squeeze(L);
-  timeseriesR=squeeze(R);
-  
-  %concatenate 3 scans
-  
-  done
-done
+for patient in 1:length(patients) 
+      for scan in 1:3
+      %load mgh surface overlap files
+      R=load_mgh('BOLDsurfrh(scan).mgh');
+      L=load_mgh('BOLDsurflh(scan).mgh');
+      
+      %created plot of data if wanted
+      %stackedplot(L);
+      
+      %pull out timeseries data from 4d array mgh
+      timeseriesL =squeeze(L);
+      timeseriesR=squeeze(R);
+    
+            if scan=1
+            %concatenate 3 scans
+            alltimeseriesL=(timeseriesL);
+            alltimeseriesR=(timeseriesR);
+            else
+            alltimeseriesL=cat(alltimeseries, timeseriesL);
+            alltimeseriesR=cat(alltimeseries, timeseriesR);
+            end
+      end
+end
