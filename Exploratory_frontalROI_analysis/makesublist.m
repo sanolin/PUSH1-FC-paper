10b9e535485ccc79843bfde392ab5d846d38295d
sanolin <150274849+sanolin@users.noreply.github.com>
2024-01-11 12:15:19 -0500

    Update makesublist.m

--- Exploratory_frontalROI_analysis/makesublist.m
+++ Exploratory_frontalROI_analysis/makesublist.m
@@ -1,15 +1,5 @@
 cfiles = dir('/Users/sara/Desktop/02_Data/P*');
 cfiles = rmfield(cfiles, {'folder', 'date', 'bytes', 'isdir', 'datenum'});
-[cfiles(1:38).unnamed] = deal([]); 
-for ii= 1:length(subs)
-    sub=cfiles(ii).name
-    timepoint=strlength(sub)
-        if timepoint==4 %timepoint 1
-              [cfiles(ii).unnamed] = deal('.pre');
-            elseif timepoint==5 %timepoint 2
-               [cfiles(ii).unnamed] = deal('.post');
-               cfiles(ii).name=sub(1:4);
-            end
-end
+
 cd('/Users/sara/Desktop/');
 writetable(struct2table(cfiles), 'sublist.txt')  
